321f4ff154d294afe8525fda00d95b7044ab151a
LuciaRyan <lcryan06@gmail.com>
2023-10-17 17:34:24 +0200

    Initial commit

---
+++ src/main/java/com/example/workingbeesapp/WorkingBeesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.workingbeesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingBeesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkingBeesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workingbeesapp/WorkingBeesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workingbeesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingBeesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
