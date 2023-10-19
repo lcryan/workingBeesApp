777119460d9e47d168bb3dfc963ae011652a5f1f
LuciaRyan <lcryan06@gmail.com>
2023-10-19 15:08:55 +0200

    set up and checked - functional

--- src/main/java/com/example/workingbeesapp/services/TeamService.java
+++ src/main/java/com/example/workingbeesapp/services/TeamService.java
@@ -1,4 +1,111 @@
 package com.example.workingbeesapp.services;
 
+import com.example.workingbeesapp.dtos.TeamDto;
+import com.example.workingbeesapp.exceptions.RecordNotFoundException;
+import com.example.workingbeesapp.models.Team;
+import com.example.workingbeesapp.repositories.TeamRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TeamService {
+
+    private final TeamRepository teamRepository;
+
+    public TeamService(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
+
+    public List<TeamDto> getAllTeams() {
+        List<Team> teams = teamRepository.findAll();
+        List<TeamDto> teamDtoList = new ArrayList<>();
+        for (Team team : teams) {
+            TeamDto teamDto = transferTeamToTeamDto(team);
+            teamDtoList.add(teamDto);
+        }
+        return teamDtoList;
+    }
+
+    // FUNCTION FOR GET ONE COMPANY //
+
+    public TeamDto getOneTeam(Long id) {
+        Optional<Team> optionalTeam = teamRepository.findById(id);
+        if (optionalTeam.isPresent()) {
+            TeamDto team = transferTeamToTeamDto(optionalTeam.get());
+            return team;
+        } else {
+            throw new RecordNotFoundException("Item of type Team with id: " + id + " could not be found.");
+        }
+    }
+
+    // FUNCTION FOR CREATING ONE COMPANY //
+
+    public TeamDto createTeam(TeamDto teamDto) {
+        Team newTeam = transferTeamDtoToTeam(teamDto);
+        teamRepository.save(newTeam);
+        return transferTeamToTeamDto(newTeam);
+    }
+
+    // FUNCTION TO UPDATE COMPANY //
+    public TeamDto updateTeam(Long id, TeamDto teamDto) {
+        if (teamRepository.findById(id).isPresent()) {
+
+            Team team = teamRepository.findById(id).get();
+
+            Team company1 = transferTeamDtoToTeam(teamDto);
+
+            company1.setId(team.getId());
+
+            teamRepository.save(company1);
+
+            return transferTeamToTeamDto(company1);
+        } else {
+            throw new RecordNotFoundException("Item of type Team with id: " + id + " could not be found.");
+        }
+    }
+
+    // FUNCTION TO DELETE COMPANY //
+    public void deleteTeam(Long id) {
+        if (teamRepository.existsById(id)) {
+            Optional<Team> optionalTeam = teamRepository.findById(id);
+            Team obsoleteTeam = optionalTeam.get();
+
+            teamRepository.delete(obsoleteTeam);
+        } else {
+            throw new RecordNotFoundException("Item of type Team with id: " + id + " could not be found.");
+        }
+    }
+
+
+    // ******* TRANSFER HELPER METHODS HERE!!!  ******* //
+
+    private TeamDto transferTeamToTeamDto(Team team) {
+
+        TeamDto teamDto = new TeamDto();
+
+        teamDto.setId(team.getId());
+        teamDto.setCompany(team.getCompany());
+        teamDto.setWorkingSpace(team.getWorkingSpace());
+        teamDto.setRoomSize(team.getRoomSize());
+        teamDto.setExtraService(team.getExtraService());
+
+        return teamDto;
+    }
+
+    private Team transferTeamDtoToTeam(TeamDto teamDto) {
+
+        Team team = new Team();
+
+        team.setId(teamDto.getId());
+        team.setCompany(teamDto.getCompany());
+        team.setWorkingSpace(teamDto.getWorkingSpace());
+        team.setRoomSize(teamDto.getRoomSize());
+        team.setExtraService(teamDto.getExtraService());
+
+        return team;
+    }
 }
+
