84e52cef74408cae292f0c53b4409aed8a8cbbb1
LuciaRyan <lcryan06@gmail.com>
2023-10-22 18:00:58 +0200

    impl. equals & hash

--- src/main/java/com/example/workingbeesapp/dtos/TeamDto.java
+++ src/main/java/com/example/workingbeesapp/dtos/TeamDto.java
@@ -1,10 +1,13 @@
 package com.example.workingbeesapp.dtos;
 
 import com.example.workingbeesapp.models.Company;
+import com.example.workingbeesapp.models.Team;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Objects;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,6 +21,7 @@ public class TeamDto {
 
     private String teamName;
 
+    // --- relation COMPANY --- //
     private Company company;
 
     private String workingSpace;
@@ -25,4 +29,17 @@ public class TeamDto {
     private int teamSize;
 
     private String extraService;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TeamDto that = (TeamDto) o;
+        return Objects.equals(id, that.id) && Objects.equals(teamName, that.teamName) && Objects.equals(company, that.company) && Objects.equals(workingSpace, that.workingSpace) && Objects.equals(teamSize, that.teamSize) && Objects.equals(extraService, that.extraService);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, teamName, company, workingSpace, teamSize, extraService);
+    }
 }
