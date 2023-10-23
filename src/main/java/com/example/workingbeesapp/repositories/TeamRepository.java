b61549f8e61d44b17d3ff0d4ede9d50fa878b1a7
LuciaRyan <lcryan06@gmail.com>
2023-10-23 12:05:21 +0200

    added List Team to access companyName when searching for teams through findTeamsByCompany_CompanyName

--- src/main/java/com/example/workingbeesapp/repositories/TeamRepository.java
+++ src/main/java/com/example/workingbeesapp/repositories/TeamRepository.java
@@ -1,7 +1,11 @@
 package com.example.workingbeesapp.repositories;
 
+import com.example.workingbeesapp.models.Company;
 import com.example.workingbeesapp.models.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    List<Team> findTeamsByCompany_CompanyName(String companyName);
 }
