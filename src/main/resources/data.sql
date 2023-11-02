29932639ef0cd4ae2adfbdafdc9c14ba22782352
LuciaRyan <lcryan06@gmail.com>
2023-11-02 11:26:33 +0100

    added one extra service and updated extraservices with team_id to test relation - functional in DB and in postman

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,23 @@
-INSERT INTO teams (id, team_name, team_size, extra_service)
-VALUES (1, 'Pixar Inc. Creative Team', 12,
-        'lunch catering'),
-       (2, 'Pixar Inc. Marketing Team', 45, 'catering breakfast');
-
-INSERT INTO subscriptions (id, price, duration, start_date, end_date, working_space_type)
-VALUES (45, 290.00, '3 months', '12/1/2024', '12/4/2024', 'Conference room');
-
-
-INSERT INTO companies (id, company_name, company_details, payment_details)
-VALUES (12, 'Pixar Inc.', 'Johnson Street 56, 896629 Calabasas, California',
-        'American Bank, 873027 89830 9878');
+INSERT INTO teams (id, team_name, team_size)
+VALUES (1, 'Pixar Inc. Creative Team', 12),
+       (2, 'Pixar Inc. Marketing Team', 45);
 
 INSERT INTO working_spaces(id, space_name, space_type, space_capacity)
 VALUES (67, 'Tokyo Valley', 'conference room', 100);
 
+INSERT INTO subscriptions (id, price, duration, start_date, end_date, working_space_type)
+VALUES (45, 290.00, '3 months', '12/1/2024', '12/4/2024', 'Conference room');
+
+INSERT INTO extra_services (id, service_name, service_type, service_price, service_duration)
+VALUES (88, 'Pixar Team Up!Lunch Buffet', 'catering buffet', 3150.00, '7 days');
+
+INSERT INTO companies (id, company_name, company_details, payment_details)
+VALUES (12, 'Pixar Inc.', 'Johnson Street 56, 896629 Calabasas, California', 'American Bank, 873027 89830 9878');
+
+
 UPDATE teams
 SET team_working_space = 67
 WHERE id = 2;
-
 
 UPDATE teams
 SET company_id = 12
@@ -31,3 +31,6 @@ UPDATE companies
 SET your_subscriptions = 45
 WHERE id = 12;
 
+UPDATE extra_services
+SET team_id = 2
+WHERE id = 88;
