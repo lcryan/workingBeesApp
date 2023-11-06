8bd6330fcace6023165148c151b69b454a79eeb5
LuciaRyan <lcryan06@gmail.com>
2023-11-06 09:59:27 +0100

    started process for upload/download file feature - two models completed for this purpose

---
+++ src/main/java/com/example/workingbeesapp/models/FileDocument.java
@@ -0,0 +1,27 @@
+package com.example.workingbeesapp.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Getter
+@Setter
+
+@Entity
+public class FileDocument {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String fileName;
+
+    @Lob
+    private byte[] docFile;
+}
