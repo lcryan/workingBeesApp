19ec71af03d98bb934077b281606d26cc39d55ab
LuciaRyan <lcryan06@gmail.com>
2023-11-07 10:08:50 +0100

    added auth dto for security set up

--- src/main/java/com/example/workingbeesapp/dtos/AuthenticationDto.java
+++ src/main/java/com/example/workingbeesapp/dtos/AuthenticationDto.java
@@ -1,2 +1,23 @@
-package com.example.workingbeesapp.dtos;public class AuthenticationDto {
+package com.example.workingbeesapp.dtos;
+
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Getter
+@Setter
+
+public class AuthenticationDto {
+
+    @NotEmpty(message = "Username cannot be empty")
+    private String username;
+
+    @NotEmpty(message = "Password cannot be empty")
+    private String password;
 }
