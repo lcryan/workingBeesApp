4ac588ec86b4f40161eb0b41964e52b86c0aeb12
LuciaRyan <lcryan06@gmail.com>
2023-11-07 08:44:37 +0100

    starting building security for app

--- src/main/java/com/example/workingbeesapp/repositories/UserRepository.java
+++ src/main/java/com/example/workingbeesapp/repositories/UserRepository.java
@@ -1,7 +1,9 @@
-/*package com.example.workingbeesapp.repositories;
+package com.example.workingbeesapp.repositories;
 
 import com.example.workingbeesapp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-}*/
+public interface UserRepository extends JpaRepository<User, String> {
+User findByUsername(String username);
+
+}
