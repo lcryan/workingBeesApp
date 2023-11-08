7fbbbb0224972ef4e018df1a997617c3db8a3e38
LuciaRyan <lcryan06@gmail.com>
2023-11-08 19:28:07 +0100

    stripped down UserDto to comply User model

--- src/main/java/com/example/workingbeesapp/dtos/UserDto.java
+++ src/main/java/com/example/workingbeesapp/dtos/UserDto.java
@@ -1,6 +1,5 @@
 package com.example.workingbeesapp.dtos;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
