038d698668dee75c3e41e495097cf7777c3f7090
LuciaRyan <lcryan06@gmail.com>
2023-11-08 14:49:41 +0100

    stripped down UserDto to username and password

--- src/main/java/com/example/workingbeesapp/dtos/UserDto.java
+++ src/main/java/com/example/workingbeesapp/dtos/UserDto.java
@@ -22,16 +22,5 @@ public class UserDto {
     @NotEmpty(message = "Password cannot be empty")
     private String password;
 
-    @NotEmpty(message = "First name cannot be empty")
-    private String firstname;
-
-    @NotEmpty(message = "Last name cannot be empty")
-    private String lastname;
-
-    @Email(message = "Email had to be a valid email address")
-    private String email;
-
-    @NotEmpty
     private String[] roleList;
-
 }
