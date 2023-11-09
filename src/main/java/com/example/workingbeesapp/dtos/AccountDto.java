42d57bf042dca4948306640d8c7e3ecac849aa92
LuciaRyan <lcryan06@gmail.com>
2023-11-09 09:58:51 +0100

    set up Account model and Account Dto for sec - removed rel. code from CompanyDto

--- src/main/java/com/example/workingbeesapp/dtos/AccountDto.java
+++ src/main/java/com/example/workingbeesapp/dtos/AccountDto.java
@@ -1,4 +1,25 @@
 package com.example.workingbeesapp.dtos;
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Getter
+@Setter
+
+
 public class AccountDto {
+
+    @NotEmpty(message = "Password cannot be empty")
+    private String password;
+    @NotEmpty(message = "Username cannot be empty")
+    private String username;
+    @NotEmpty(message = "A role has to be assigned")
+    private String[] roleList;
+
 }
--- src/main/java/com/example/workingbeesapp/dtos/CompanyDto.java
+++ src/main/java/com/example/workingbeesapp/dtos/CompanyDto.java
@@ -17,23 +17,16 @@ import java.util.List;
 
 public class CompanyDto {
 
-    // company related fields //
-    public Long id; // TODO: check if this is needed //
+    public Long id;
+
     @NotEmpty(message = "Company name cannot be empty")
     private String companyName;
+
     @NotEmpty(message = "Company details cannot be empty")
     private String companyDetails;
+
     @NotEmpty(message = "Payment details cannot be empty")
     private String paymentDetails;
-
-    // this is for security purposes - user and role related //
-    @NotEmpty(message = "Password cannot be empty")
-    private String password;
-    @NotEmpty(message = "Username cannot be empty")
-    private String username;
-    @NotEmpty(message = "A role has to be assigned")
-    private String[] roleList;
-    // end security purposes //
 
     // --- TEAMS of relation One-To-Many ---  COMPANY IS ONE - TEAM IS MANY //
     private List<TeamDto> teams;
