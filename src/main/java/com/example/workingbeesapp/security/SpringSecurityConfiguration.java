c2d08e92cf07f0925b3a483e042ba9f6813d0ddc
LuciaRyan <lcryan06@gmail.com>
2023-11-09 14:15:50 +0100

    security functional on postman - endpoints working according to set up sec - midday push on 9.11.2023 for big merge

--- src/main/java/com/example/workingbeesapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/workingbeesapp/security/SpringSecurityConfiguration.java
@@ -51,69 +51,69 @@ public class SpringSecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-// defining the authorization rules //
+                        // defining the authorization rules below per entity //
 
 
-                                // for authentication //
-                                .requestMatchers(HttpMethod.POST, "/authentication").permitAll()
+                        // for authentication //
+                        .requestMatchers(HttpMethod.POST, "/authentication").permitAll()
 
-                                // for users //
-                                .requestMatchers(HttpMethod.POST, "/users").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/users").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.GET, "/users/{id}").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.PUT, "/users/{id}").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.DELETE, "/users/{id}").hasRole("ADMIN")
+                        // for users //
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/users").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/users/{id}").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.PUT, "/users/{id}").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.DELETE, "/users/{id}").hasRole("ADMIN")
 
-                                // for roles //
-                                .requestMatchers(HttpMethod.GET, "/roles").hasRole("ADMIN")
+                        // for roles //
+                        .requestMatchers(HttpMethod.GET, "/roles").hasRole("ADMIN")
 
-                                // for accounts //
-                                .requestMatchers(HttpMethod.POST, "/accounts").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.GET, "/accounts").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.GET, "/accounts/{id}").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.PUT, "/accounts/{id}").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.DELETE, "/accounts/{id}").hasRole("ADMIN")
+                        // for accounts //
+                        .requestMatchers(HttpMethod.POST, "/accounts").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.GET, "/accounts").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/accounts/{id}").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.PUT, "/accounts/{id}").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.DELETE, "/accounts/{id}").hasRole("ADMIN")
 
-                                // for companies //
-                                .requestMatchers(HttpMethod.POST, "/companies").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.GET, "/companies").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.GET, "/companies/{id}").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.PUT, "/companies/{id}").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.DELETE, "/companies/{id}").hasRole("ADMIN")
+                        // for companies //
+                        .requestMatchers(HttpMethod.POST, "/companies").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.GET, "/companies").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/companies/{id}").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.PUT, "/companies/{id}").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.DELETE, "/companies/{id}").hasRole("ADMIN")
 
-                                // for working spaces //
-                                .requestMatchers(HttpMethod.POST, "/workingSpaces").hasRole("ADMIN") // only admin assigns working spaces //
-                                .requestMatchers(HttpMethod.GET, "/workingSpaces").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.GET, "/workingSpaces/{workingSpaceId}").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.PUT, "/workingSpaces/{workingSpaceId}").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.DELETE, "/workingSpaces/{workingSpaceId}").hasRole("ADMIN")
+                        // for working spaces //
+                        .requestMatchers(HttpMethod.POST, "/workingSpaces").hasRole("ADMIN") // only admin assigns working spaces //
+                        .requestMatchers(HttpMethod.GET, "/workingSpaces").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/workingSpaces/{workingSpaceId}").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.PUT, "/workingSpaces/{workingSpaceId}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/workingSpaces/{workingSpaceId}").hasRole("ADMIN")
 
-                                // for subscriptions //
-                                .requestMatchers(HttpMethod.POST, "/subscriptions").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.GET, "/subscriptions").hasAnyRole("ADMIN")
-                                .requestMatchers(HttpMethod.GET, "/subscriptions/{subscriptionId}").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.PUT, "/subscriptions/{subscriptionId}").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.DELETE, "/subscriptions/{subscriptionId}").hasRole("ADMIN")
+                        // for subscriptions //
+                        .requestMatchers(HttpMethod.POST, "/subscriptions").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/subscriptions").hasAnyRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/subscriptions/{subscriptionId}").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.PUT, "/subscriptions/{subscriptionId}").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.DELETE, "/subscriptions/{subscriptionId}").hasRole("ADMIN")
 
-                                // for extra services //
-                                .requestMatchers(HttpMethod.POST, "/extraservices").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.GET, "/extraservices").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.GET, "/extraservices/{extraServiceId}").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.PUT, "/extraservices/{extraServiceId}").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.DELETE, "/extraservices/{extraServiceId}").hasRole("ADMIN")
+                        // for extra services //
+                        .requestMatchers(HttpMethod.POST, "/extraservices").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.GET, "/extraservices").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.GET, "/extraservices/{extraServiceId}").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.PUT, "/extraservices/{extraServiceId}").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.DELETE, "/extraservices/{extraServiceId}").hasRole("ADMIN")
 
-                                // for file upload and download //
-                                .requestMatchers(HttpMethod.POST, "/single/fileUpload").hasRole("ADMIN") // single upload
-                                .requestMatchers(HttpMethod.GET, "/download/{fileName}").hasAnyRole("ADMIN", "USER") // single download
+                        // for file upload and download //
+                        .requestMatchers(HttpMethod.POST, "/single/fileUpload").hasRole("ADMIN") // single upload
+                        .requestMatchers(HttpMethod.GET, "/download/{fileName}").hasAnyRole("ADMIN", "USER") // single download
 
-                                // for teams //
-                                .requestMatchers(HttpMethod.POST, "/teams").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.GET, "/teams").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.GET, "/teams/{teamId}").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.PUT, "/teams/{teamId}").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers(HttpMethod.DELETE, "/teams/{teamId}").hasAnyRole("ADMIN", "USER")
+                        // for teams //
+                        .requestMatchers(HttpMethod.POST, "/teams").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.GET, "/teams").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.GET, "/teams/{teamId}").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.PUT, "/teams/{teamId}").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.DELETE, "/teams/{teamId}").hasAnyRole("ADMIN", "USER")
 
-                                .anyRequest().permitAll()
+                        .anyRequest().permitAll()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(csrf -> csrf.disable())
