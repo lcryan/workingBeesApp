2c97f8be29a557ef7502f6f5e11c0fece3ccf453
LuciaRyan <lcryan06@gmail.com>
2023-11-09 10:09:12 +0100

    set up Account repo

--- src/main/java/com/example/workingbeesapp/repositories/AccountRepository.java
+++ src/main/java/com/example/workingbeesapp/repositories/AccountRepository.java
@@ -1,4 +1,8 @@
 package com.example.workingbeesapp.repositories;
 
-public class AccountRepository {
+import com.example.workingbeesapp.models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account findByUsername(String username);
 }
