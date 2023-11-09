20adbaaf162ff6265b25328056cdae0172e72ee9
LuciaRyan <lcryan06@gmail.com>
2023-11-09 11:55:08 +0100

    adjusted user service to new sec set up with account as a one to one relation

--- src/main/java/com/example/workingbeesapp/services/UserService.java
+++ src/main/java/com/example/workingbeesapp/services/UserService.java
@@ -1,10 +1,13 @@
 package com.example.workingbeesapp.services;
 
+import com.example.workingbeesapp.dtos.AccountDto;
 import com.example.workingbeesapp.dtos.CompanyDto;
 import com.example.workingbeesapp.dtos.UserDto;
+import com.example.workingbeesapp.models.Account;
 import com.example.workingbeesapp.models.Company;
 import com.example.workingbeesapp.models.Role;
 import com.example.workingbeesapp.models.User;
+import com.example.workingbeesapp.repositories.AccountRepository;
 import com.example.workingbeesapp.repositories.CompanyRepository;
 import com.example.workingbeesapp.repositories.RoleRepository;
 import com.example.workingbeesapp.repositories.UserRepository;
@@ -22,14 +25,14 @@ public class UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
 
-    private final CompanyRepository companyRepository;
+    private final AccountRepository accountRepository;
 
 
-    public UserService(PasswordEncoder passwordEncoder, UserRepository userRepository, RoleRepository roleRepository, CompanyRepository companyRepository) {
+    public UserService(PasswordEncoder passwordEncoder, UserRepository userRepository, RoleRepository roleRepository, AccountRepository accountRepository) {
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
-        this.companyRepository = companyRepository;
+        this.accountRepository = accountRepository;
     }
 
     // GET ALL USERS //
@@ -47,23 +50,6 @@ public class UserService {
         return userDtos;
     }
 
-    // ------------------------------------------------------ //
-
-/*    public String createUser(UserDto userDto) {
-        User newUser = new User();
-        newUser.setUsername(userDto.getUsername());
-        newUser.setPassword(passwordEncoder.encode(userDto.getPassword()));
-
-        List<Role> userRoles = new ArrayList<>();
-        for (String roleName : userDto.getRoleList()) {
-            Optional<Role> optionalRole = roleRepository.findById("ROLE_" + roleName);
-            optionalRole.ifPresent(userRoles::add);
-        }
-        userRepository.save(newUser);
-
-        return "User successfully created";
-    }*/
-
     // HELPER METHODS USER TO USER DTO AND USER DTO TO USER //
     private static void userToUserDto(User user, UserDto userDto) {
         userDto.setUsername(user.getUsername());
@@ -75,7 +61,6 @@ public class UserService {
         }
         userDto.setRoleList(roleList.toArray(new String[0]));
     }
-
     private static void userDtoToUser(User user, UserDto userDto) {
         user.setUsername(userDto.getUsername());
         user.setPassword(userDto.getPassword());
@@ -83,33 +68,33 @@ public class UserService {
 
     // ------------------------------------------------------ //
 
-    // Create USER with Company and Role //
-    public UserDto createUserWithCompany(CompanyDto companyDto) {
-
-        // Create a new user for company //
+    public UserDto createUserWithAccount(AccountDto accountDto) {
 
         UserDto userDto = new UserDto();
-        userDto.setUsername(companyDto.getUsername());
-        userDto.setPassword(passwordEncoder.encode(companyDto.getPassword()));
+        userDto.setUsername(accountDto.getUsername());
+        userDto.setPassword(passwordEncoder.encode(accountDto.getPassword()));
 
         User user = new User();
-        if (companyDto.getRoleList() != null) {
+        if (accountDto.getRoleList() != null) {
             List<Role> userRoles = new ArrayList<>();
-            for (String roleName : companyDto.getRoleList()) {
+            for (String roleName : accountDto.getRoleList()) {
                 Optional<Role> optionalRole = roleRepository.findById("ROLE_" + roleName);
                 optionalRole.ifPresent(userRoles::add);
             }
-            // making a new User //
             userDtoToUser(user, userDto);
             user.setRoleList(userRoles);
         }
 
-        // Create a new company-user //
-        Company company = new Company();
-        companyDtoToCompany(companyDto, company);
-        // save both in repo //
-        companyRepository.save(company);
+        Account account = new Account();
+        accountDtoToAccount(accountDto, account);
+
+        accountRepository.save(account);
         userRepository.save(user);
+
+        user.setAccount(account);
+
+        userRepository.save(user);
+        accountRepository.save(account);
 
         UserDto savedUserDto = new UserDto();
         userToUserDto(user, savedUserDto);
@@ -117,13 +102,12 @@ public class UserService {
         return savedUserDto;
     }
 
-    // make CompanyDto to Company //
-
-    private void companyDtoToCompany(CompanyDto companyDto, Company company) {
-        company.setCompanyName(companyDto.getCompanyName());
-        company.setUsername(companyDto.getUsername());
-        company.setPassword(companyDto.getPassword());
-        company.setRoleList(List.of(companyDto.getRoleList()));
+    // Transfer method AccountDto to Account //
+    private void accountDtoToAccount(AccountDto accountDto, Account account) {
+        account.setCompanyName(accountDto.getCompanyName());
+        account.setUsername(accountDto.getUsername());
+        account.setPassword(accountDto.getPassword());
+        account.setRoleList(List.of(account.getRoleList().toString()));
     }
 }
 
