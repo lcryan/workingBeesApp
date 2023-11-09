e9cfc8b0f1752cca4402a9fbd5b057114cc2867a
LuciaRyan <lcryan06@gmail.com>
2023-11-09 10:10:00 +0100

    added TODO

--- src/main/java/com/example/workingbeesapp/repositories/AccountRepository.java
+++ src/main/java/com/example/workingbeesapp/repositories/AccountRepository.java
@@ -4,5 +4,5 @@ import com.example.workingbeesapp.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Account findByUsername(String username);
+    Account findByUsername(String username); // TODO : check if this is absolutely necessary //
 }
