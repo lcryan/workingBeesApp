14f81b62a74165ee08b9d1d86bfb7e330fb29c19
LuciaRyan <lcryan06@gmail.com>
2023-11-13 15:46:25 +0100

    deleted unused import

--- src/test/java/com/example/workingbeesapp/controllers/ExtraServiceControllerTest.java
+++ src/test/java/com/example/workingbeesapp/controllers/ExtraServiceControllerTest.java
@@ -19,7 +19,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(ExtraServiceController.class)
