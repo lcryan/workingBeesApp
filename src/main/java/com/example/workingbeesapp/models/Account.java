1e4b01395fae58f05ec27c2c389ea961390f1242
LuciaRyan <lcryan06@gmail.com>
2023-11-13 19:53:39 +0100

    added lombok for constructors - was missing here

--- src/main/java/com/example/workingbeesapp/models/Account.java
+++ src/main/java/com/example/workingbeesapp/models/Account.java
@@ -1,10 +1,13 @@
 package com.example.workingbeesapp.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
 
 @Getter
 @Setter
