6d6795c935ca3e42a193650ea95402b9747ec4c2
LuciaRyan <lcryan06@gmail.com>
2023-11-16 12:10:29 +0100

    deleted unused code

--- src/main/java/com/example/workingbeesapp/exceptions/IdNotFoundException.java
+++ src/main/java/com/example/workingbeesapp/exceptions/IdNotFoundException.java
@@ -5,8 +5,4 @@ public class IdNotFoundException extends RuntimeException {
     public IdNotFoundException(String message) {
         super(message);
     }
-
-    public IdNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
