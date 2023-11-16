3a384de302ddd8324d3f033500852680c88c0986
LuciaRyan <lcryan06@gmail.com>
2023-11-16 15:23:46 +0100

    formatted and changed comments

--- src/main/java/com/example/workingbeesapp/controllers/WorkingSpaceController.java
+++ src/main/java/com/example/workingbeesapp/controllers/WorkingSpaceController.java
@@ -21,7 +21,6 @@ import java.util.Optional;
 @RequestMapping("/workingspaces")
 public class WorkingSpaceController {
     private final WorkingSpaceService workingSpaceService;
-
     private final FileStorageService fileStorageService;
 
     public WorkingSpaceController(WorkingSpaceService workingSpaceService, FileStorageService fileStorageService) {
@@ -29,9 +28,9 @@ public class WorkingSpaceController {
         this.fileStorageService = fileStorageService;
     }
 
-    // GET LIST OF WORKING SPACES ALPHABETICALLY SORTED BY COMPANY NAME, IF COMPANY NAME APPLICABLE //
+    //--- get all working spaces by company name, if applicable---//
     @GetMapping("")
-    @Transactional // this is needed to be able to link an image to the WorkingSpace //
+    @Transactional
     public ResponseEntity<List<WorkingSpaceDto>> getWorkingSpacesByCompanyName(@RequestParam(value = "companyName", required = false) Optional<String> companyName) {
         List<WorkingSpaceDto> workingSpaceDtos;
         if (companyName.isEmpty()) {
@@ -44,7 +43,7 @@ public class WorkingSpaceController {
         return ResponseEntity.ok().body(workingSpaceDtos);
     }
 
-    // GET ONE TEAM //
+    //--- get working space ---//
     @GetMapping("/{id}")
     @Transactional // see comment above //
     public ResponseEntity<WorkingSpaceDto> getWorkingSpace(@PathVariable Long id) {
@@ -53,7 +52,7 @@ public class WorkingSpaceController {
     }
 
 
-    // CREATE TEAM --- CHECKED//
+    //--- create working space ---//
     @PostMapping("")
     public ResponseEntity<Object> createNewWorkingSpace(@Validated @RequestBody WorkingSpaceDto workingSpaceDto, BindingResult bindingResult) {
         if (bindingResult.hasFieldErrors()) {
@@ -71,13 +70,14 @@ public class WorkingSpaceController {
         }
     }
 
-    // UPDATE WORKING SPACE//
+    //--- update working space ---//
     @PutMapping("/{id}")
     public ResponseEntity<WorkingSpaceDto> updateWorkingSpace(@PathVariable Long id, @Validated @RequestBody WorkingSpaceDto newTeam) {
         WorkingSpaceDto workingSpaceDto1 = workingSpaceService.updateWorkingSpace(id, newTeam);
         return ResponseEntity.ok().body(workingSpaceDto1);
     }
 
+    //--- delete working space ---//
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteWorkingSpace(@PathVariable Long id) {
         workingSpaceService.deleteWorkingSpace(id);
@@ -85,12 +85,14 @@ public class WorkingSpaceController {
         return ResponseEntity.noContent().build();
     }
 
+    //--- assign subscription to working space ---//
     @PutMapping("/{id}/{subscriptionId}")
     public ResponseEntity<Object> assignSubscriptionToWorkingSpace(@PathVariable("id") Long id, @PathVariable("subscriptionId") Long subscriptionId) {
         workingSpaceService.assignSubscriptionToWorkingSpace(id, subscriptionId);
         return ResponseEntity.noContent().build();
     }
 
+    //--- assign image to working space ---//
     @PostMapping("/{id}/image")
     public ResponseEntity<Object> assignImageToWorkingSpace(@PathVariable("id") Long id, @RequestBody MultipartFile multipartFile) {
         String url = ServletUriComponentsBuilder.fromCurrentContextPath().path("/download/").path(Objects.requireNonNull(multipartFile.getOriginalFilename())).toUriString();
